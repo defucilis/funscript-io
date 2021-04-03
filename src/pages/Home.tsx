dbab98725ad70759ec907c2ce6b5a7a8485ff7a8
Defucilis <defucilis@gmail.com>
2021-04-04 00:09:09 +1100

    feat: Improve Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,15 +3,21 @@ import React from "react";
 import Layout from "../components/layout/Layout";
 import StackedContentPage from "../components/layout/StackedContentPage";
 
+import style from "./Home.module.scss"
+
 const Home = () => {
     return (
         <Layout>
             <StackedContentPage>
-                <h1>Funscript.io</h1>
-                <p>
-                    This site contains a variety of tools and utilities for the creation,
-                    modification and consumption of Funscripts
-                </p>
+                <div className={style.home}>
+                    <img src="logo512.png" width={300} height={300} alt="Funscript.io Logo" />
+                    <h1>Funscript.io</h1>
+                    <ul>
+                        <li>Create Funscripts</li>
+                        <li>Modify Funscripts</li>
+                        <li>Enjoy Funscripts</li>
+                    </ul>
+                </div>
             </StackedContentPage>
         </Layout>
     );
