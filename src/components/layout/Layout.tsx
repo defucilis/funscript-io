b0b9b253b672479b57ed9b156f6d953bf865865c
Defucilis <defucilis@gmail.com>
2021-04-03 23:03:52 +1100

    feat: Restrict site to 900px wide screens or wider

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,17 +1,33 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
 import style from "./Layout.module.scss";
+import MobileWarning from "./MobileWarning";
 
 const Layout = ({ children }: { children: ReactNode }) => {
+
+    const [wideEnough, setWideEnough] = useState(true);
+
+    useEffect(() => {
+        const handleResize = () => setWideEnough(window.innerWidth > 900);
+
+        window.addEventListener("resize", handleResize);
+        setWideEnough(window.innerWidth > 900);
+
+        return () => window.removeEventListener("resize", handleResize);
+    })
+
     return (
         <>
             <Header />
             <div className={style.layout}>
-                <Sidebar />
-                <div className={style.container}>{children}</div>
+                { wideEnough ? <Sidebar /> : null }
+                { wideEnough 
+                    ? <div className={style.container}>{children}</div>
+                    : <MobileWarning />
+                }
             </div>
         </>
     );
