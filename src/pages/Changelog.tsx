954ecd733a941e9c7456f90a7069f7bd13a1b43f
Defucilis <defucilis@gmail.com>
2021-04-18 19:53:04 +1000

    feat: Update changelog

--- src/pages/Changelog.tsx
+++ src/pages/Changelog.tsx
@@ -12,6 +12,15 @@ const Changelog = (): JSX.Element => {
                 <h1>Changelog</h1>
                 <div className={style.changelog}>
                     <div>
+                        <h2>v0.7.0</h2>
+                        <p className={style.date}>18th April 2021</p>
+                        <h3>Added</h3>
+                        <ul>
+                            <li>Funscripts can now have their metadata edited in the Modify page</li>
+                            <li>Added ScriptAxis top scripts browser to the Browse page!</li>
+                        </ul>
+                    </div>
+                    <div>
                         <h2>v0.6.1</h2>
                         <p className={style.date}>18th April 2021</p>
                         <h3>Fixed</h3>
