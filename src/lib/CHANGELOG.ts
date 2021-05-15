ba9a1a193d5943d7aeccb351faa980d27a06e45f
Defucilis <defucilis@gmail.com>
2021-05-15 20:41:48 +1000

    feat: Improve changelog editability

---
+++ src/lib/CHANGELOG.ts
@@ -0,0 +1,116 @@
+//there has to be a better way than this... :I
+const changelog = `
+## v0.7.3
+
+### 15th May 2021
+
+#### Changed
+
+  * Made the changelog easier to update!
+
+#### Fixed
+
+  * Fixed a bug preventing scripts from appearing in the Browse section
+
+---
+
+## v0.7.2
+
+### 29th April 2021
+
+#### Fixed
+
+  * Fixed a bug where under certain circumstances, funscripts would report invalid values for Average Speed
+  * Made it possible to refresh the page when in sub-pages (like /modify) without getting a 404 error
+
+---
+
+## v0.7.1
+
+### 28th April 2021
+
+#### Fixed
+
+  * Solved a crash when attempting to edit the metadata of a script without existing performers or tags keys
+
+---
+
+## v0.7.0
+
+### 18th April 2021
+
+#### Added
+
+  * Funscripts can now have their metadata edited in the Modify page
+  * Added ScriptAxis top scripts browser to the Browse page!
+
+---
+
+## v0.6.1
+
+### 18th April 2021
+
+#### Fixed
+
+  * Funscript popups now filter for .funscript files (suggestion by spuzz1127)
+
+---
+
+## v0.6.0
+
+### 16th April 2021
+
+#### Added (suggestions by spuzz1127)
+
+  * Video playback can now be toggled by clicking the video
+  * Clicking the heatmap preview in the local player seeks through the video
+  * Funscripts can be previewed during playback by clicking the small funscript button on the bottom right of the player
+  * Doesn't work in fullscreen mode
+
+---
+
+## v0.5.0
+
+### 15th April 2021
+
+#### Added
+
+  * Added Limiter modifier to ensure a script matches a device's capabilities
+  * Space bar now pauses/unpauses the local script player
+  * Left/Right arrow keys now seek 10s back/forward in the local script player (suggestion by spuzz1127)
+  * It is now possible to change the stroke speed/length increment amounts in manual mode (suggestion by Jupiter)
+  * Added this changelog page!
+
+#### Changed
+
+  * Up/Down arrow keys are now used instead of left/right to change sync offset in the local script player
+
+#### Fixed
+
+  * Heatmaps now show gaps in the funscript (rather than continuing the color that was before the gap)
+
+---
+
+## v0.4.1
+
+### 11th April 2021
+
+#### Fixed
+
+  * Stopped overwriting funscript metadata, oops! (thanks sentinel)
+
+---
+
+## v0.4.0
+
+### 4th April 2021
+
+#### Features
+
+  * A better local-video script player than handyfeeling.com
+  * Easy to use script-modification features
+  * A better manual-mode interface
+  * Procedural funscript generation
+`;
+
+export default changelog;
--- src/pages/Changelog.tsx
+++ src/pages/Changelog.tsx
@@ -1,136 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ReactMarkdown from "react-markdown";
 import Layout from "../components/layout/Layout";
 import StackedContentPage from "../components/layout/StackedContentPage";
+
+import ChangelogContent from "../lib/CHANGELOG";
 
 import style from "./Changelog.module.scss";
 
 //this is a temporary version!
 const Changelog = (): JSX.Element => {
+    useEffect(() => {
+        console.log("fetching");
+        fetch("./test.txt").then(val => {
+            val.text().then(text => {
+                console.log(text);
+            });
+        });
+    }, []);
+
     return (
         <Layout>
             <StackedContentPage>
                 <h1>Changelog</h1>
                 <div className={style.changelog}>
-                    <div>
-                        <h2>v0.7.2</h2>
-                        <p className={style.date}>29th April 2021</p>
-                        <h3>Fixed</h3>
-                        <ul>
-                            <li>
-                                Fixed a bug where under certain circumstances, funscripts would
-                                report invalid values for Average Speed
-                            </li>
-                            <li>
-                                Made it possible to refresh the page when in sub-pages (like
-                                /modify) without getting a 404 error
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h2>v0.7.1</h2>
-                        <p className={style.date}>28th April 2021</p>
-                        <h3>Fixed</h3>
-                        <ul>
-                            <li>
-                                Solved a crash when attempting to edit the metadata of a script
-                                without existing performers or tags keys
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h2>v0.7.0</h2>
-                        <p className={style.date}>18th April 2021</p>
-                        <h3>Added</h3>
-                        <ul>
-                            <li>
-                                Funscripts can now have their metadata edited in the Modify page
-                            </li>
-                            <li>Added ScriptAxis top scripts browser to the Browse page!</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h2>v0.6.1</h2>
-                        <p className={style.date}>18th April 2021</p>
-                        <h3>Fixed</h3>
-                        <ul>
-                            <li>
-                                Funscript popups now filter for .funscript files (suggestion by
-                                spuzz1127)
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h2>v0.6.0</h2>
-                        <p className={style.date}>16th April 2021</p>
-                        <h3>Added (suggestions by spuzz1127)</h3>
-                        <ul>
-                            <li>Video playback can now be toggled by clicking the video</li>
-                            <li>
-                                Clicking the heatmap preview in the local player seeks through the
-                                video
-                            </li>
-                            <li>
-                                Funscripts can be previewed during playback by clicking the small
-                                funscript button on the bottom right of the player
-                                <br />
-                                Doesn&apos;t work in fullscreen mode
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h2>v0.5.0</h2>
-                        <p className={style.date}>15th April 2021</p>
-                        <h3>Added</h3>
-                        <ul>
-                            <li>
-                                Added Limiter modifier to ensure a script matches a device&apos;s
-                                capabilities
-                            </li>
-                            <li>Space bar now pauses/unpauses the local script player</li>
-                            <li>
-                                Left/Right arrow keys now seek 10s back/forward in the local script
-                                player (suggestion by spuzz1127)
-                            </li>
-                            <li>
-                                It is now possible to change the stroke speed/length increment
-                                amounts in manual mode (suggestion by Jupiter)
-                            </li>
-                            <li>Added this changelog page!</li>
-                        </ul>
-                        <h3>Changed</h3>
-                        <ul>
-                            <li>
-                                Up/Down arrow keys are now used instead of left/right to change sync
-                                offset in the local script player
-                            </li>
-                        </ul>
-                        <h3>Fixed</h3>
-                        <ul>
-                            <li>
-                                Heatmaps now show gaps in the funscript (rather than continuing the
-                                color that was before the gap)
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h2>v0.4.1</h2>
-                        <p className={style.date}>11th April 2021</p>
-                        <h3>Fixed</h3>
-                        <ul>
-                            <li>Stopped overwriting funscript metadata, oops! (thanks sentinel)</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h2>v0.4.0</h2>
-                        <p className={style.date}>4th April 2021</p>
-                        <h3>Features</h3>
-                        <ul>
-                            <li>A better local-video script player than handyfeeling.com</li>
-                            <li>Easy to use script-modification features</li>
-                            <li>A better manual-mode interface</li>
-                            <li>Procedural funscript generation</li>
-                        </ul>
-                    </div>
+                    <ReactMarkdown children={ChangelogContent} />
                 </div>
             </StackedContentPage>
         </Layout>
